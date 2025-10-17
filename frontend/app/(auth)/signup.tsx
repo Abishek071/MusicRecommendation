37965797e5006c607ed1841b0271dd458900dc19
Abishek071 <abishekbca2078@prime.edu.np>
2025-10-17 15:26:11 +0545

    Signup page connected with backend

--- frontend/app/(auth)/signup.tsx
+++ frontend/app/(auth)/signup.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import { useColorScheme, View, Text, TextInput, Pressable } from "react-native";
-import { Colors, Fonts } from "@/constants/theme"; // your file
+import { Colors, Fonts } from "@/constants/theme";
+import { useRouter } from "expo-router";
 
 export default function Signup() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const scheme = useColorScheme(); // "light" | "dark" | null
   const c = Colors[scheme ?? "light"];
+
+  const api = "http://127.0.0.1:8000";
+
+  const registerUser = async () => {
+    try {
+      if (email == "" && password == "" && name == "") {
+        alert("Please fill the form completely");
+        return;
+      }
+      const res = await fetch(`${api}/api/auth/register/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password, display_name: name }),
+      });
+
+      if (!res.ok) throw new Error("Register failed");
+      router.push("/(tabs)");
+    } catch (error) {
+      console.log("Error while registering user:", error);
+    }
+  };
 
   return (
     <View
@@ -45,6 +72,22 @@ export default function Signup() {
         <TextInput
           placeholder="Enter your email"
           placeholderTextColor="#9CA3AF"
+          onChangeText={(text) => setEmail(text)}
+          style={{
+            color: c.text,
+            borderWidth: 1,
+            borderColor: "#d1d5db",
+            borderRadius: 12,
+            paddingVertical: 16,
+            paddingHorizontal: 16,
+            fontFamily: Fonts.sans,
+            marginBottom: 15,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          placeholderTextColor="#9CA3AF"
+          onChangeText={(text) => setName(text)}
           style={{
             color: c.text,
             borderWidth: 1,
@@ -60,6 +103,7 @@ export default function Signup() {
           placeholder="Create a password"
           placeholderTextColor="#9CA3AF"
           secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
           style={{
             color: c.text,
             borderWidth: 1,
@@ -88,6 +132,7 @@ export default function Signup() {
         />
 
         <Pressable
+          onPress={() => registerUser()}
           style={{
             backgroundColor: c.icon,
             borderRadius: 12,
